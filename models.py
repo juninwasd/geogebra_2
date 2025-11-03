52ed62f292a1e3bbff6d569c89ff013e77cb573a
junin <juniomoreirabrito@gmail.com>
2025-11-03 19:34:24 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,21 @@
+class Point:
+    def __init__(self, x, y, name=None):
+        self.x = float(x)
+        self.y = float(y)
+        self.name = name
+
+class Line:
+    # defined by two points (references)
+    def __init__(self, p1: Point, p2: Point):
+        self.p1 = p1
+        self.p2 = p2
+
+class Circle:
+    def __init__(self, center: Point, radius: float):
+        self.center = center
+        self.radius = float(radius)
+
+class PlotFunc:
+    def __init__(self, expr: str):
+        self.expr = expr  # string expression, evaluated with x in locals
 
